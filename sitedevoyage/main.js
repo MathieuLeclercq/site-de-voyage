46e6574238d43784d64abfecd68f0c8210204aa8
MathieuLeclercq <56450839+MathieuLeclercq@users.noreply.github.com>
2019-10-28 19:14:21 +0100

    Update main.js

--- sitedevoyage/main.js
+++ sitedevoyage/main.js
@@ -1,21 +1,21 @@
 ﻿var destinations = {
     "pt": ["Portugal", 200, true, true, false, true],
-    "it": ["Italie", 100, true, true, true, false],
-    "ir": ["Irlande", 300, true, true, true, true],
+    "it": ["Italie", 100, true, true, false, false],
+    "ir": ["Irlande", 300, true, true, true, false],
     "jp": ["Japon", 800, true, true, false, false],
-    "us": ["Etats-Unis", 1100, true, true, false, true],
+    "us": ["Etats-Unis", 1100, true, true, false, false],
     "es": ["Espagne", 50, true, true, true, false],
-    "lis": ["Lisbonne", 200, true, true, false, true],
+    "lis": ["Lisbonne", 200, true, true, false, false],
     "lon": ["Londres", 500, true, true, true, false],
-    "dub": ["Dublin", 400, true, true, true, true],
+    "dub": ["Dublin", 400, true, true, false, true],
     "nyc": ["New York", 1100, true, true, false, false],
-    "sin": ["Singapour", 800, true, true, false, true],
-    "par": ["Paris", 50, true, true, true, false]
+    "sin": ["Singapour", 800, true, true, false, false],
+    "par": ["Paris", 50, true, true, false, false]
 };
 
 function filtre() {
     for (var i in destinations) {
-        if ((document.getElementById("prix").value > destinations[i][1]) || ((document.getElementById("dejeuner").checked) && (destinations[i][4] == true))) {
+        if ((document.getElementById("prix").value > destinations[i][1]) || ((document.getElementById("dejeuner").checked) && (destinations[i][4] == true))|| ((document.getElementById("animaux").checked) && (destinations[i][5] == true)))  {
                 document.getElementById(i).style.opacity = "0.30";
                 document.getElementById(i).removeAttribute("href");
             } else {
